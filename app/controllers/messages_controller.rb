858708befeff2c1510c2bc750b098104f4b87f13
momchiltues19 <mom.todorov.tues19@gmail.com>
2017-11-04 18:23:38 +0200

    migrated

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   def show
-    	
+    @message = Message.find(params[:id])	
   end
   def new
-    
   end
   def create
-    
+    @message = Message.new(params.require(:messages).permit(:text))
+    @message.save
   end
 end
