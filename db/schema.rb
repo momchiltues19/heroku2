d19b56701556cb87c8a2363072a798955a539b7d
momchiltues19 <mom.todorov.tues19@gmail.com>
2017-11-04 18:43:04 +0200

    fix_routes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20171104160153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
