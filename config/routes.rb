7697b0c288156397a320a953d393520a19198f82
momchiltues19 <mom.todorov.tues19@gmail.com>
2017-11-04 18:30:29 +0200

    fix_pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'messages' => 'messages#new'
-  resources :messages
-  root 'messages#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
