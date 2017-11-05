a875256f8fef8f19d9d8c2c4cb94b1cbfb4e1443
momchiltues19 <mom.todorov.tues19@gmail.com>
2017-11-05 22:37:23 +0200

    final_fix_part_un

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,24 @@
 require 'json'
 
 class MessagesController < ApplicationController
-	protect_from_forgery except: :api
-	def new
-	end
-	def create
-		@message = Message.new(params.require(:messages).permit(:text))
-		@message.save
-		url = "https://cipher-me.herokuapp.com/messages/" + @message.id.to_s
-		render plain: url		
-	end
-	def show
-		@message = Message.find(params[:id])
-		@message.destroy
-	end
-	def api
-		json = params.permit(:message)
-		@message = Message.new
-		@message.text = json
-		@message.save
-		url = "https://cipher-me.herokuapp.com/messages/" + @message.id.to_s
-		url_json = {:url => url}  	
-		render json: url_json.to_json
-	end
-end
+  protect_from_forgery except: :api
+  def show
+    @message = Message.find(params[:id])
+    @message.destroy 	
+  end
+  def new
+  end
+  def create
+    @message = Message.new(params.require(:message).permit(:text))
+    @message.save 
+    url = "https://privnote-mine.herokuapp.com/messages/" + @message.id.to_s
+    render plain: url
+  end
+  def api 
+    @message = Mesasge.new
+    @message.text = params.permit(:message)
+    @message.save
+    url = "https://privnote-mine.herokuapp.com/messages/" + @messages.id.to_s
+    url_json = {:url => url}
+    render json: url_json.to_json
+  end 
