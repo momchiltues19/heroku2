5c9467a69b7d9ea758c33f3f8437d74630392d8c
momchiltues19 <mom.todorov.tues19@gmail.com>
2017-11-05 21:25:17 +0200

    json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
+require 'json'
+
 class MessagesController < ApplicationController
+  protect_from_forgery except: :api
   def show
     @message = Message.find(params[:id])
     @message.destroy 	
@@ -8,5 +11,14 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(params.require(:message).permit(:text))
     @message.save 
+    url = "https://privnote-mine.herokuapp.com/messages" + @messages.id.to_s
+    render plain: url
   end
+  def api 
+    @message = Mesasge.new
+    @message.text = params.permit(:message)
+    @message.save
+    url = "https://privnote-mine.herokuapp.com/messages" + @messages.id.to_s
+    render json: {:url => url}
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'messages' => 'messages#new'
   resources :messages
   root 'messages#new'
+  post 'notes/api' => 'messages#api'
 end
